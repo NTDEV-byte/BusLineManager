5e5624daf26314288c581f75a18ba01b92cf3908
bookman <nassim.t998@gmail.com>
2022-06-29 22:23:35 +0200

    [BLM-Simulation-State-Pattern] ChargeBusState

---
+++ src/application/back/simulation/state/ChargePassagersBusState.java
@@ -0,0 +1,16 @@
+package application.back.simulation.state;
+
+import application.back.simulation.items.BusSimulation;
+
+public class ChargePassagersBusState extends BusState{
+
+    public ChargePassagersBusState(BusSimulation bus) {
+        super(bus);
+    }
+
+    @Override
+    public void display() {
+        System.out.println(bus.toString()+ " est entrain de charger des passagers à l'arret: "+bus.getArretPresent().toString());
+    }
+
+}
