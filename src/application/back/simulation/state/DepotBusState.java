dea13322b26405a07640af53922de39af3ceb720
bookman <nassim.t998@gmail.com>
2022-06-29 22:23:54 +0200

    [BLM-Simulation-State-Pattern] DepotBusState

---
+++ src/application/back/simulation/state/DepotBusState.java
@@ -0,0 +1,16 @@
+package application.back.simulation.state;
+
+import application.back.simulation.items.BusSimulation;
+
+public class DepotBusState extends BusState{
+
+    public DepotBusState(BusSimulation bus) {
+        super(bus);
+    }
+
+
+    @Override
+    public void display() {
+        System.out.println(bus.toString()+" est dans le dépot");
+    }
+}
